500ca34568c342719510be7e7d0edab9b1b0c171
Cerebrovinny <vinny.cardoso96@gmail.com>
2020-01-28 15:38:15 -0300

    added binomial file

---
+++ binomial_distribution.R
@@ -0,0 +1,17 @@
+dbinom(3,5,0.5) #calculate probability, how is the (x) number of sucess = 3, size of experiment = 5 times, prob of sucess 50%
+
+#4 semaphores of 4 times, prob = 0,1,2,3,4
+dbinom(0,4,0.25) #prob of any semaphore green = 0, size of experiment 4, prob of green semaphore 0.25
+
+dbinom(1,4,0.25) #prob of 1 semaphore green = 1, size of experiment 4, prob of green semaphore 0.25
+
+dbinom(2,4,0.25) #prob of 2 semaphore green = 2, size of experiment 4, prob of green semaphore 0.25
+
+dbinom(3,4,0.25) #prob of 3 semaphore green = 3, size of experiment 4, prob of green semaphore 0.25
+
+dbinom(4,4,0.25) #prob of 4 semaphore green = 4, size of experiment 4, prob of green semaphore 0.25
+
+#Pass in USP first exam
+#exam have 90 questions, 5 options
+
+dbinom(73,90,0.20)
